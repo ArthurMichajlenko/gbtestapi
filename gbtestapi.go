e548e897cf6fe2eb72afae14901f76d79fd70192
Arthur Michajlenko <michajlenko1967@gmail.com>
2019-09-18 15:24:40 +0300

    Start

---
+++ gbtestapi.go
@@ -0,0 +1,19 @@
+/*
+ * Filename: /Users/arthur/go/src/gbtestapi/gbtestapi.go
+ * Path: /Users/arthur/go/src/gbtestapi
+ * Created Date: Wednesday, September 18th 2019, 3:13:21 pm
+ * Author: Arthur Michajlenko
+ * 
+ * Copyright (c) 2019 Infinit Loop SRL
+ */
+
+
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Test")
+}
