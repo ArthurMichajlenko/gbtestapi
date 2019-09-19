fb2ce79618169468d000c009975a38cf2ae63873
Arthur Michajlenko <michajlenko1967@gmail.com>
2019-09-19 08:00:36 +0300

    Begin tview

--- gbtestapi.go
+++ gbtestapi.go
@@ -3,17 +3,23 @@
  * Path: /Users/arthur/go/src/gbtestapi
  * Created Date: Wednesday, September 18th 2019, 3:13:21 pm
  * Author: Arthur Michajlenko
- * 
+ *
  * Copyright (c) 2019 Infinit Loop SRL
  */
-
 
 package main
 
 import (
 	"log"
+
+	"github.com/rivo/tview"
 )
 
 func main() {
-	log.Println("Test1")
+	log.Println("Start...")
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	// app:=tview.NewApplication()
+	if err:= tview.NewApplication().SetRoot(box,true).Run(); err != nil {
+		panic(err)
+	}
 }
