6b34b9ca5d78823f519c9aa4540917e4d04d21f1
Arthur Michajlenko <michajlenko1967@gmail.com>
2019-10-12 22:12:34 +0300

    MySQL

--- gbtestapi.go
+++ gbtestapi.go
@@ -101,8 +101,10 @@ func main() {
 		ordersList = append(ordersList, order)
 	}
 	for _, order := range ordersList {
-		_, err := db.NamedExec(`INSERT INTO orders (id, courier_id, client_id, product_to, product_from, payment_method, quantity_to, quantity_from, order_cost, order_status, delivery_delay, date_start, date_finish)
-			 VALUES (:id, :courier_id, :client_id, :product_to, :product_from, :payment_method, :quantity_to, :quantity_from, :order_cost, :order_status, :delivery_delay, :date_start, :date_finish)`, &order)
+		_, err := db.NamedExec(`INSERT INTO orders (id, courier_id, client_id, product_to, product_from, payment_method, quantity_to, quantity_from, order_cost, order_status, delivery_delay, date_start)
+			 VALUES (:id, :courier_id, :client_id, :product_to, :product_from, :payment_method, :quantity_to, :quantity_from, :order_cost, :order_status, :delivery_delay, :date_start)`, &order)
+		// _, err := db.NamedExec(`INSERT INTO orders (id, courier_id, client_id, product_to, product_from, payment_method, quantity_to, quantity_from, order_cost, order_status, delivery_delay, date_start, date_finish)
+		// 	 VALUES (:id, :courier_id, :client_id, :product_to, :product_from, :payment_method, :quantity_to, :quantity_from, :order_cost, :order_status, :delivery_delay, :date_start, :date_finish)`, &order)
 		if err != nil {
 			log.Println(err)
 		}
